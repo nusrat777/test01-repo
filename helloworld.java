542d0a158278b90557fa9bf7c845ffc1a0890478
nusrat <nusratabdullah999@gmail.com>
2024-02-18 22:29:54 -0500

    Nusrat's helloworld java program

---
+++ helloworld.java
@@ -0,0 +1,5 @@
+Public class HelloWorld {# this program is written by nusrat
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}// end of the program
